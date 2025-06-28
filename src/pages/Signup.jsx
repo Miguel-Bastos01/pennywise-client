90b012626f64b583dd640d73179614bac6e6013d
Miguel Bastos <miguel.alexandre.bastos01@gmail.com>
2025-06-28 10:12:35 +0100

    Signup page initiated

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Signup(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const navigate = useNavigate()
+
+    const handleSignup = (e) => {
+        e.preventDefault()
+
+        axios
+         .post("http://localhost:5005/auth/signup", {email, password, name})
+         .then((res) => {
+            navigate("/login")
+         })
+         .catch((err) => {
+            setErrorMessage(err.response?.data?.message || "Signup unsuccessful")
+         })
+    }
+
+    return(
+        <form onSubmit={handleSignup} className="max-w-sm mx-auto">
+            <h2 className="text-lg font-semibold mb-4">Create an Account</h2>
+
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className="w-full border px-3 py-2 mb-2"/>
+
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full border px-3 py-2 mb-2"/>
+
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full border px-3 py-2 mb-2"/>
+
+              <button type="submit" className="w-full bg-black text-white py-2 rounded">
+                Sign up
+              </button>
+
+              {errorMessage && (
+                <p className="text-red-500 mt-2 text-sm">{errorMessage}</p>
+              )}
+        </form>
+    )
+}
+
+export default Signup
